6995c36175e82fcd36b9397da89ab12e6d05b765
Kokoroou <66433773+Kokoroou@users.noreply.github.com>
2022-06-28 23:41:07 +0700

    Optimize code and delete unnecessary files

---
+++ README_vi.md
@@ -0,0 +1,38 @@
+# Portrait Generator
+
+## Giới thiệu
+Tạo bức vẽ chân dung nghệ thuật mới từ khuôn mặt thật bằng Dual Style GAN
+
+## Tính năng chính
+- [x] Chọn phong cách nghệ thuật cho bức vẽ
+- [x] Điều chỉnh cấu trúc và màu sắc của bức vẽ
+- [ ] Cá nhân hóa phong cách vẽ
+
+## Yêu cầu
+Đã cài đặt python 3.9 và pip
+
+## Cài đặt
+1. Clone [repository này](https://github.com/Kokoroou/portrait-generator)
+2. Tải thư mục [checkpoint](https://drive.google.com/drive/folders/1rAiKfCO_fcQsbnPDu04DgJ_iATdIQLHz?usp=sharing)
+và lưu trong thư mục vừa clone
+3. Chạy file `install.py` trong đường dẫn **src/config/**
+4. Chạy file `main.py` trong đường dẫn **src/**
+
+## Sử dụng
+1. Trong trang web vừa hiện lên, chuyển qua tab **Generator**
+2. Upload ảnh chân dung của bản thân 
+3. Chọn style theo ý muốn (Mặc định là **Random**)
+4. Điều chỉnh các tham số để được màu ảnh phù hợp
+5. Chờ hệ thống tạo ảnh mới
+6. Lưu ảnh vào máy
+
+## Credit
+Thành viên của nhóm:
+- [Trương Tuấn Anh](https://github.com/Kokoroou)
+- [Vũ Quốc Công](https://github.com/congth2001)
+- [Nguyễn Nam Dương](https://github.com/duongnn194034)
+- [Vũ Huy Hoàng](https://github.com/Gohan0107)
+- [Nguyễn Minh Hoàng](https://github.com/ltkz000)
+
+## Giấy phép
+GNU General Public License v3.0
--- src/model/gen_style.py
+++ src/model/gen_style.py
@@ -1,114 +1,139 @@
+import os
 import random
-from model.dualstylegan import DualStyleGAN
-from src.controller.GANController import *
-import torch
-from PIL import Image
-from src.global_var import *
 from argparse import Namespace
+
+from model.dualstylegan import DualStyleGAN
 from model.encoder.psp import pSp
 from model.sampler.icp import ICPTrainer
+from src.controller.GANController import *
+from src.global_var import *
 
 styles = ['cartoon', 'caricature', 'anime', 'arcane', 'comic', 'pixar', 'slamdunk']
 
 
-def gen_style(gene, style_id=0):
-    info = {}
-
-    # Select style type
-    if style_id == -1:
-        style_id = random.randint(0, len(styles)-1)
-    style = styles[style_id]
-
-    print("Create new image with style", style)
-
+class StyleGenerator(object):
     # MODEL_DIR = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'checkpoint')  # Stand-alone
     MODEL_DIR = os.path.join(os.path.dirname(os.getcwd()), 'checkpoint')  # Streamlit
-    weight = [0.75] * 7 + [1] * 11
 
-    if os.path.exists(os.path.join(MODEL_DIR, style, 'refined_exstyle_code.npy')):
-        exstyle_name = 'refined_exstyle_code.npy'
-    else:
-        exstyle_name = 'exstyle_code.npy'
+    def __init__(self):
+        self.style = ""
+        self.src_image_tensor = None
+        self.encoder = None
+        self.generator = None
+        self.gene = None
+        self.weight = None
 
-    # Load DualStyleGAN
-    generator = DualStyleGAN(size=1024, style_dim=512, n_mlp=8, channel_multiplier=2, res_index=6)
-    generator.eval()
-    ckpt = torch.load(os.path.join(MODEL_DIR, style, 'generator.pt'), map_location=lambda storage, loc: storage)
-    generator.load_state_dict(ckpt["g_ema"])
-    generator = generator.to(device)
+    def load_encoder(self):
+        model_path = os.path.join(self.MODEL_DIR, 'encoder.pt')
+        ckpt = torch.load(model_path, map_location='cpu')
+        opts = ckpt['opts']
+        opts['checkpoint_path'] = model_path
+        opts = Namespace(**opts)
+        opts.device = device
+        encoder = pSp(opts)
+        encoder.eval()
+        self.encoder = encoder.to(device)
 
-    # Load encoder
-    model_path = os.path.join(MODEL_DIR, 'encoder.pt')
-    ckpt = torch.load(model_path, map_location='cpu')
-    opts = ckpt['opts']
-    opts['checkpoint_path'] = model_path
-    opts = Namespace(**opts)
-    opts.device = device
-    encoder = pSp(opts)
-    encoder.eval()
-    encoder = encoder.to(device)
+    def load_dual_style_gan(self):
+        if not self.style:
+            raise Exception("Style for generator isn't added!")
 
-    # # Load extrinsic style code
-    # exstyles = np.load(os.path.join(MODEL_DIR, style, exstyle_name), allow_pickle=True).item()
+        generator = DualStyleGAN(size=1024, style_dim=512, n_mlp=8, channel_multiplier=2, res_index=6)
+        generator.eval()
+        ckpt = torch.load(os.path.join(self.MODEL_DIR, self.style, 'generator.pt'),
+                          map_location=lambda storage, loc: storage)
+        generator.load_state_dict(ckpt["g_ema"])
+        self.generator = generator.to(device)
 
-    # Load sampler network
-    icptc = ICPTrainer(np.empty([0, 512 * 11]), 128)
-    icpts = ICPTrainer(np.empty([0, 512 * 7]), 128)
-    ckpt = torch.load(os.path.join(MODEL_DIR, style, 'sampler.pt'), map_location=lambda storage, loc: storage)
-    icptc.icp.netT.load_state_dict(ckpt['color'])
-    icpts.icp.netT.load_state_dict(ckpt['structure'])
-    icptc.icp.netT = icptc.icp.netT.to(device)
-    icpts.icp.netT = icpts.icp.netT.to(device)
+    def load_sampler_network(self):
+        if not self.style:
+            raise Exception("Style for generator isn't added!")
 
-    # Source image for exstyle
-    src_images = np.load(os.path.join(MODEL_DIR, style, exstyle_name), allow_pickle=True).item()
-    src_image_names = list(src_images.keys())
-    image_index = random.randint(0, len(src_image_names))
-    src_image_name = src_image_names[image_index]
+        icptc = ICPTrainer(np.empty([0, 512 * 11]), 128)
+        icpts = ICPTrainer(np.empty([0, 512 * 7]), 128)
+        ckpt = torch.load(os.path.join(self.MODEL_DIR, self.style, 'sampler.pt'),
+                          map_location=lambda storage, loc: storage)
+        icptc.icp.netT.load_state_dict(ckpt['color'])
+        icpts.icp.netT.load_state_dict(ckpt['structure'])
+        icptc.icp.netT = icptc.icp.netT.to(device)
+        icpts.icp.netT = icpts.icp.netT.to(device)
+        self.icptc = icptc
+        self.icpts = icpts
 
-    # # Add encoder
-    # encoder = pSp(opts)  # Most time-consuming
-    # encoder.eval()
-    # encoder.to(device)
+    def modified_generator(self, style_index=-1, src_image_index=-1, structure_rate=0.0, color_rate=0.0):
+        # Save style name
+        if style_index == -1:
+            style_index = random.randrange(0, len(styles))
+        elif style_index < -1 or style_index >= len(styles):
+            raise Exception(f"Style with index {style_index} doesn't exist!")
 
-    # # Add generator
-    # generator = DualStyleGAN(size=1024, style_dim=512, n_mlp=8, channel_multiplier=2, res_index=6)
-    # generator.eval()
+        # Check if style is changed
+        if self.style != styles[style_index]:
+            self.style = styles[style_index]
 
-    # # Add instyle, exstyle
-    # latent = torch.tensor(src_images[src_image_name]).to(device)
-    # instyle = list(torch.randn(6, 512).to(device))
-    # exstyle = generator.generator.style(latent.reshape(latent.shape[0] * latent.shape[1], latent.shape[2])).reshape(
-    #     latent.shape)
+            # Modify generator and sampler network
+            self.load_dual_style_gan()
+            self.load_sampler_network()
 
-    with torch.no_grad():
-        img_rec, instyle = encoder(gene, randomize_noise=False, return_latents=True,
-                                   z_plus_latent=True, return_z_plus_latent=True, resize=False)
-        img_rec = torch.clamp(img_rec.detach(), -1, 1)
+        if os.path.exists(os.path.join(self.MODEL_DIR, self.style, 'refined_exstyle_code.npy')):
+            exstyle_name = 'refined_exstyle_code.npy'
+        else:
+            exstyle_name = 'exstyle_code.npy'
 
-        latent = torch.tensor(src_images[src_image_name]).repeat(2, 1, 1).to(device)
-        # latent[0] for both color and structrue transfer and latent[1] for only structrue transfer
-        latent[1, 7:18] = instyle[0, 7:18]
-        exstyle = generator.generator.style(latent.reshape(latent.shape[0] * latent.shape[1], latent.shape[2])).reshape(
-            latent.shape)
+        # Save tensor of source image
+        src_images = np.load(os.path.join(self.MODEL_DIR, self.style, exstyle_name), allow_pickle=True).item()
+        src_image_names = list(src_images.keys())
+        if src_image_index == -1:
+            src_image_index = random.randrange(0, len(src_image_names))
+        elif src_image_index < -1 or src_image_index >= len(src_image_names):
+            raise (Exception(f"Source image with index {src_image_index} doesn't exist!"))
+        self.src_image_name = src_image_names[src_image_index]
+        self.src_image_tensor = src_images[self.src_image_name]
 
-        img_gen, _ = generator([instyle.repeat(2, 1, 1)], exstyle, z_plus_latent=True,
-                               truncation=0.7, truncation_latent=0, use_res=True, interp_weights=[0.6] * 7 + [1] * 11)
-        img_gen = torch.clamp(img_gen.detach(), -1, 1)
-        # deactivate color-related layers by setting w_c = 0
-        img_gen2, _ = generator([instyle], exstyle[0:1], z_plus_latent=True,
-                                truncation=0.7, truncation_latent=0, use_res=True, interp_weights=[0.6] * 7 + [0] * 11)
-        img_gen2 = torch.clamp(img_gen2.detach(), -1, 1)
+        # Save weight
+        self.weight = [structure_rate] * 7 + [color_rate] * 11
 
-    # Add all info to pass to next steps
-    info["generator"] = generator
-    info["encoder"] = encoder
-    info["instyle"] = [instyle.repeat(2, 1, 1)]
-    info["exstyle"] = exstyle
+    def add_gene(self, gene):
+        self.gene = gene
 
-    new_gene = img_gen[1]
+    def generate(self):
+        if not self.encoder:
+            self.load_encoder()
+        if not self.generator:
+            self.load_dual_style_gan()
+        if not isinstance(self.gene, torch.Tensor):
+            raise Exception("Gene for generator isn't added")
 
-    return new_gene, info
+        # Print current state to screen
+        print(f"Generate new portrait with style {self.style}:\n"
+              f"--Source style image: {self.src_image_name}\n")
+
+        with torch.no_grad():
+            img_rec, instyle = self.encoder(self.gene, randomize_noise=False, return_latents=True,
+                                            z_plus_latent=True, return_z_plus_latent=True, resize=False)
+            img_rec = torch.clamp(img_rec.detach(), -1, 1)
+
+            latent = torch.tensor(self.src_image_tensor).repeat(2, 1, 1).to(device)
+            # latent[0] for both color and structure transfer and latent[1] for only structure transfer
+            latent[1, 7:18] = instyle[0, 7:18]
+            exstyle = self.generator.generator.style(
+                latent.reshape(latent.shape[0] * latent.shape[1], latent.shape[2])).reshape(
+                latent.shape)
+
+            img_gen, _ = self.generator([instyle.repeat(2, 1, 1)], exstyle, z_plus_latent=True,
+                                        truncation=0.7, truncation_latent=0, use_res=True,
+                                        interp_weights=[0.6] * 7 + [1] * 11)
+            img_gen = torch.clamp(img_gen.detach(), -1, 1)
+            # deactivate color-related layers by setting w_c = 0
+            img_gen2, _ = self.generator([instyle], exstyle[0:1], z_plus_latent=True,
+                                         truncation=0.7, truncation_latent=0, use_res=True,
+                                         interp_weights=[0.6] * 7 + [0] * 11)
+            img_gen2 = torch.clamp(img_gen2.detach(), -1, 1)
+
+        new_gene = img_gen[1]
+
+        return new_gene
+
 
 if __name__ == "__main__":
     # Generate a sample gene for everyone to understand
@@ -119,9 +144,13 @@ if __name__ == "__main__":
     gene = image_to_gene(img)
     # print(gene.size())
 
-    new_gene, _ = gen_style(gene)
+    generator = StyleGenerator()
+    generator.modified_generator()
+    generator.add_gene(gene)
+
+    next_gene = generator.generate()
     # print(new_gene.size())
 
-    image = gene_to_image(new_gene)
+    image = gene_to_image(next_gene)
     image.show()
     # show_image(image)
